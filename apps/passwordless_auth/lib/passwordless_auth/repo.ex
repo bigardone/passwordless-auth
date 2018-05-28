6bd4b598bd04183838d226e00e5dfe91dc104cea
Ricardo Garcia Vega <bigardone@gmail.com>
2018-05-28 08:39:02 +0200

    repo and test

---
+++ apps/passwordless_auth/lib/passwordless_auth/repo.ex
@@ -0,0 +1,53 @@
+defmodule PasswordlessAuth.Repo do
+  @moduledoc """
+  Emails and authentication tokens in-memory repository
+  """
+  use GenServer
+
+  require Logger
+
+  @name __MODULE__
+  @token_max_age :timer.minutes(5)
+
+  def start_link(opts) do
+    {emails, opts} =
+      opts
+      |> Keyword.put_new(:name, @name)
+      |> Keyword.pop(:emails)
+
+    GenServer.start_link(__MODULE__, emails, opts)
+  end
+
+  def valid_email?(pid \\ @name, email), do: GenServer.call(pid, {:valid_email, email})
+
+  def save(pid \\ @name, email, token),
+    do: GenServer.call(pid, {:save, email, token})
+
+  def fetch(pid \\ @name, email), do: GenServer.call(pid, {:fetch, email})
+
+  @impl GenServer
+  def init(emails) do
+    state = Enum.reduce(emails, %{}, &Map.put(&2, &1, nil))
+
+    {:ok, state}
+  end
+
+  @impl GenServer
+  def handle_call({:valid_email, email}, _from, state) do
+    Logger.info(fn -> ">> Repo: handling {:find_email, #{email}}" end)
+
+    {:reply, Map.has_key?(state, email), state}
+  end
+
+  def handle_call({:save, email, token}, _from, state) do
+    Logger.info(fn -> ">> Repo: handling {:save_token, #{email}, #{token}}" end)
+
+    {:reply, :ok, Map.put(state, email, token)}
+  end
+
+  def handle_call({:fetch, email}, _from, tokens) do
+    Logger.info(fn -> ">> Repo: handling {:fetch, #{email}}" end)
+
+    {:reply, Map.fetch(tokens, email), tokens}
+  end
+end
---
+++ apps/passwordless_auth/test/passwordless_auth/repo_test.exs
@@ -0,0 +1,65 @@
+defmodule PasswordlessAuth.RepoTest do
+  use ExUnit.Case, async: true
+
+  alias PasswordlessAuth.Repo
+
+  describe ".init/1" do
+    test "it starts the repo" do
+      name = :repo_test_1
+      {:ok, _pid} = Repo.start_link(name: name, emails: [])
+      assert true
+    end
+  end
+
+  describe ".valid_email?/2" do
+    test "returns true when passed email is in the repo's state" do
+      name = :repo_test_2
+      email = "foo@test.com"
+      {:ok, _pid} = Repo.start_link(name: name, emails: [email])
+
+      assert Repo.valid_email?(name, email)
+    end
+
+    test "returns false when passed email no it repo's state" do
+      name = :repo_test_3
+      email = "foo@test.com"
+      {:ok, _pid} = Repo.start_link(name: name, emails: [email])
+
+      refute Repo.valid_email?(name, "not_found@test.com")
+    end
+  end
+
+  describe ".save/3" do
+    test "returns :ok and sets token value in state" do
+      name = :repo_test_6
+      email = "foo@test.com"
+      token = "token-value"
+      {:ok, _pid} = Repo.start_link(name: name, emails: [email])
+
+      assert :ok = Repo.save(name, email, token)
+      assert %{"foo@test.com" => ^token} = :sys.get_state(name)
+    end
+  end
+
+  describe ".fetch/2" do
+    test "returns {:ok, token} for passed email" do
+      name = :repo_test_4
+      email = "foo@test.com"
+      token = "token-value"
+      {:ok, _pid} = Repo.start_link(name: name, emails: [email])
+      :ok = Repo.save(name, email, token)
+
+      assert {:ok, ^token} = Repo.fetch(name, email)
+    end
+
+    test "returns :error when token not found" do
+      name = :repo_test_5
+      email = "foo@test.com"
+      token = "token-value"
+      {:ok, _pid} = Repo.start_link(name: name, emails: [email])
+      :ok = Repo.save(name, email, token)
+
+      assert :error = Repo.fetch(name, "not_found@test.com")
+    end
+  end
+end
